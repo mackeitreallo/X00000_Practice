cbe277bd196fd7d6aab47bd9cd85768133f86371
mackeitreallo <markrenzcarillo@gmail.com>
2020-12-29 14:10:01 +0000

    3rd Exercise done

--- 2_Understanding_Data_Types_and_How_to_Manipulate_Strings/Exercise3.py
+++ 2_Understanding_Data_Types_and_How_to_Manipulate_Strings/Exercise3.py
@@ -1,1 +1,6 @@
 #Mathematical Operations in Python
+print("Body Mass Index Calculator")
+bmiw = float(input("\nWhat is your weight in kilograms?\n"))
+bmih = float(input("\nHow about your height in meters?\n"))
+bmi = int(bmiw / (bmih **2))
+print("\nYour Body Mass Index is: " + str(bmi))
