63896e7b3825f11eb9e9a671cf4b60a68fcb5ae6
mackeitreallo <markrenzcarillo@gmail.com>
2020-12-29 19:38:49 +0000

    2nd Exercise done

--- 3_Control_Flow_and_Logical_Operators/Exercise2.py
+++ 3_Control_Flow_and_Logical_Operators/Exercise2.py
@@ -1,1 +1,18 @@
 #Nested if statements and elif statements
+print("\nBody Mass Index Calculator Ver.X")
+wt = float(input("\nWhat is your weight in kg? "))
+ht = float(input("What is your height in m? "))
+bmi = round(wt / (ht ** 2), 2)
+if bmi >= 18.5:
+    if bmi >= 25:
+        if bmi >= 30:
+            if bmi >= 35:
+                print(f"Your BMI is {bmi}, you should go and see a doctor.")
+            else:
+                print(f"Your BMI is {bmi}, you are obese.")
+        else:
+            print(f"Your BMI is {bmi}, you are slightly overweight.")
+    else:
+        print(f"Your BMI is {bmi}, you are perfectly normal!")
+else:
+    print(f"Your BMI is {bmi}, you need nutrients!")
