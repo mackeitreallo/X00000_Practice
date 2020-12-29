503011176ccdb1cc6e722e5fc85b8d10833f59a1
mackeitreallo <markrenzcarillo@gmail.com>
2020-12-29 17:51:03 +0000

    1st Exercise done

--- 3_Control_Flow_and_Logical_Operators/Exercise1.py
+++ 3_Control_Flow_and_Logical_Operators/Exercise1.py
@@ -1,1 +1,8 @@
 #Control Flow with if  else and Conditional Operators
+number = int(input("Enter what number you want to check: "))
+check = number % 2
+
+if check == 0:
+    print("\nThe number is even.")
+else:
+    print("\nThe number is odd.")
