c1840a3e4bdb1549ed931b4f9b8f3a1db1f2c17d
mackeitreallo <markrenzcarillo@gmail.com>
2020-12-30 14:59:11 +0000

    Final Application done

--- 3_Control_Flow_and_Logical_Operators/Final_Application.py
+++ 3_Control_Flow_and_Logical_Operators/Final_Application.py
@@ -1,14 +1,14 @@
 #Minimalist Treasure Island!
 print("\nWelcome to the Treasure Island Budget Friendly Version!")
 
-choice1 = input("\nDo you want to go to the left or right? Type 'Left' or 'Right': ")
-if choice1 == "Left":
+choice1 = input("\nDo you want to go to the left or right? Type 'Left' or 'Right': ").lower()
+if choice1 == "left":
     print("\nAwesome mate!")
-    choice2 = input("The coast is near and there are 3 suspicious doors, swim or wait? Type 'Swim' or 'Wait': ")
-    if choice2 == "Wait":
+    choice2 = input("The coast is near and there are 3 suspicious doors, swim or wait? Type 'Swim' or 'Wait': ").lower()
+    if choice2 == "wait":
         print("\nNot bad boiiiiii!")
-        choice3 = input("\nWe're at the end game now, choose 1 door amongst the 3. Blue, Red or White?: ")
-        if choice3 == "Blue" or choice3 == "Red":
+        choice3 = input("\nWe're at the end game now, choose 1 door amongst the 3. Blue, Red or White?: ").lower()
+        if choice3 == "blue" or choice3 == "red":
             print("\nGame Over!")
         else:
             print("\nYou Win!")
