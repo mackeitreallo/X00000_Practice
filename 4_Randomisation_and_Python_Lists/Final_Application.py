bb0bbe7752f37c89e52115b28b8eda38fa98da11
mackeitreallo <markrenzcarillo@gmail.com>
2021-01-01 08:59:17 +0000

    Final Exercise done

--- 4_Randomisation_and_Python_Lists/Final_Application.py
+++ 4_Randomisation_and_Python_Lists/Final_Application.py
@@ -14,32 +14,39 @@ weapon = [choice0, choice1, choice2]
 
 #Player Weapon Choosing
 player_weapon = int(input("\nEnter 0 for Rock, 1 for paper and 2 for scissors!: "))
-player = weapon[player_weapon]
 
-#Machine Weapon Choosing
-machine_weapon = [choice0, choice1, choice2]
-machine = random.choice(machine_weapon)
+#Invalid Results
+if player_weapon < 0 or player_weapon > 2:
+    print("\nRock, Paper, Scissors Only!")
 
-#Battle Bets
-output = player + " vs " + machine
-
-#Conditions
-result1 = f"{choice0} vs {choice2}"
-result2 = f"{choice1} vs {choice0}"
-result3 = f"{choice2} vs {choice1}"
-result4 = f"{choice2} vs {choice0}"
-result5 = f"{choice0} vs {choice1}"
-result6 = f"{choice1} vs {choice2}"
-
-#Result Message
-print(f"\nPlayer = {output} = Machine")
-
-#Battle Results
-if output == result1 or output == result2 or output == result3:
-    print("\nPlayer Wins!")
-elif output == result4 or output == result5 or output == result6:
-    print("\nMachine Wins!")
+#Resume Game
 else:
-    print("\nGame is a Draw!")
+    player = weapon[player_weapon]
+
+    #Machine Weapon Choosing
+    machine_weapon = [choice0, choice1, choice2]
+    machine = random.choice(machine_weapon)
+
+    #Battle Bets
+    output = player + " vs " + machine
+
+    #Conditions
+    result1 = f"{choice0} vs {choice2}"
+    result2 = f"{choice1} vs {choice0}"
+    result3 = f"{choice2} vs {choice1}"
+    result4 = f"{choice2} vs {choice0}"
+    result5 = f"{choice0} vs {choice1}"
+    result6 = f"{choice1} vs {choice2}"
+
+    #Battle Results
+    if output == result1 or output == result2 or output == result3:
+        print("\nPlayer Wins!")
+    elif output == result4 or output == result5 or output == result6:
+        print("\nMachine Wins!")
+    else:
+        print("\nGame is a Draw!")
+
+    #Battle Message
+    print(f"\nPlayer = {output} = Machine")
     
     
