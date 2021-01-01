b6a35808ccfbc5953f8f259b169e27368cdd58e2
mackeitreallo <markrenzcarillo@gmail.com>
2021-01-01 15:52:40 +0000

    3rd Exercise done

--- 5_Python_Loops/Exercise3.py
+++ 5_Python_Loops/Exercise3.py
@@ -1,1 +1,8 @@
 #for loops and the range() function
+
+evens = 0
+for numbers in range (1, 102, 2):
+    #print(numbers - 1)
+    subtracted = numbers - 1
+    evens += subtracted
+print(f"\n{evens}")
