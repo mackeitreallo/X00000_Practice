c34628696203044e07327809cb8302436af2c55d
mackeitreallo <markrenzcarillo@gmail.com>
2021-01-01 07:00:52 +0000

    2nd Exercise done

---
+++ 4_Randomisation_and_Python_Lists/Exercise2.py
@@ -0,0 +1,17 @@
+#Understanding the Offset and Appending Items to Lists
+import random
+
+print("\nThis is not KKB, who will be the next payer for the bill game!")
+
+#Results Override
+RNGGOD = int(input("\nEnter the number you want to control the results: "))
+random.seed(RNGGOD)
+
+#Bill Roullete
+candidates = input("\nWho are the peasants that shall be the candidates to pay the bill?: ")
+name = candidates.split(", ")
+
+range = len(name)
+
+choice = random.randint(0, range)
+print(f"\n{name[choice]} will pay the bill. No more questions!")
