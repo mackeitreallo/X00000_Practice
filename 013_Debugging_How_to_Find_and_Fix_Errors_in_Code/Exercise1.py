c165449b62b9b7beb50c477c1fc2211f62ad68ca
mackeitreallo <markrenzcarillo@gmail.com>
2021-01-09 17:47:28 +0000

    1st Exercise done

--- 013_Debugging_How_to_Find_and_Fix_Errors_in_Code/Exercise1.py
+++ 013_Debugging_How_to_Find_and_Fix_Errors_in_Code/Exercise1.py
@@ -1,2 +1,16 @@
-#test again test from cloud 9
-#test again from localhost
+#Original Code to Check
+
+#number = int(input("Which number do you want to check?"))
+#
+#if number % 2 = 0:
+#  print("This is an even number.")
+#else:
+#  print("This is an odd number.")
+
+#My Solution
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
