bfd02ac53acf1891ccd92044e4542fc8d9a6584d
mackeitreallo <markrenzcarillo@gmail.com>
2021-01-09 17:51:23 +0000

    2nd Exercise done

---
+++ 013_Debugging_How_to_Find_and_Fix_Errors_in_Code/Exercise2.py
@@ -0,0 +1,28 @@
+#Original Code to Check
+#
+#year = input("Which year do you want to check?")
+#
+#if year % 4 == 0:
+#  if year % 100 == 0:
+#    if year % 400 == 0:
+#      print("Leap year.")
+#    else:
+#      print("Not leap year.")
+#  else:
+#    print("Leap year.")
+#else:
+#  print("Not leap year.")
+  
+#My Solution
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
