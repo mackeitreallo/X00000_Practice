1673fb9bf6c160cfe2fe47ad490b0d73b9d91ee8
mackeitreallo <markrenzcarillo@gmail.com>
2021-01-11 23:52:23 +0800

    added sample link

--- 014_Higher_Lower_Game_Project/Final_Application.py
+++ 014_Higher_Lower_Game_Project/Final_Application.py
@@ -1,3 +1,7 @@
+#sample link: https://repl.it/@appbrewery/higher-lower-final
+
 import random
 from replit import clear
 import Final_Application_Database
+
+print(Final_Application_Database.splash_logo)
