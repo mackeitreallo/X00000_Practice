bd50e730266adebf2bb05ec1de39965de688bde2
mackeitreallo <markrenzcarillo@gmail.com>
2021-01-23 11:58:06 +0800

    Final Application done

--- 016_Object_Oriented_Programming/Final_Application.py
+++ 016_Object_Oriented_Programming/Final_Application.py
@@ -1,3 +1,22 @@
-from menu import Menu, Menuitem
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+import menu
+import money_machine
+import coffee_maker
+
+x = money_machine.MoneyMachine()
+y = coffee_maker.CoffeeMaker()
+z = menu.Menu()
+is_on = True
+
+while is_on:
+    options = z.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        y.report()
+        x.report()
+    else:
+        drink = z.find_drink(choice)
+        if y.is_resource_sufficient(drink):
+            if x.make_payment(drink.cost):
+                y.make_coffee(drink)
